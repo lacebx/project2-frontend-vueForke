f1981dbeb669b209bd9a03d3c9d2b463b8c35d53
lacebx <arsenemanzi@pm.me>
2024-10-26 13:31:41 -0400

    edited

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/2024/project2/t8/',
+  base: '/2024/project2/t8/',//added this line
   plugins: [
     vue(),
   ],
